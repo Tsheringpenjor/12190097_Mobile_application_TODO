1f93fa79c72ef2b41306d98d0d2c484942c2aa7b
Tshering Penjor <tsheringpenjor@Tsherings-MacBook-Air.local>
2021-05-28 11:52:38 +0600

    TODO_23

---
+++ TODO_23/app/src/main/java/com/example/todo_23/CustomReceiver.java
@@ -0,0 +1,30 @@
+package com.example.todo_23;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+
+        if(intentAction != null){
+            String toastMessage = "Unknown Intent Action";
+            switch (intentAction){
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage  = "Power Connected";
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage = "Power Disconnected";
+                    break;
+            }
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+}
